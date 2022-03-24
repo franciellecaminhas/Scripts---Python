4f58eb2fa7970f9f1e7758cd1f247b879882627d
Francielle Caminhas <franciellegonsi@hotmail.com>
2022-03-24 08:58:53 -0300

    primeiro commit

---
+++ desafio02.py
@@ -0,0 +1,5 @@
+print('=====Desafio 02=====')
+dia = input('Qual é o dia que você nasceu?')
+mes = input('Qual é o mês que você nasceu?')
+ano = input('Qual é o ano que você nasceu?')
+print('Você nasceu no dia',dia,'de',mes,'de',ano,'!')
